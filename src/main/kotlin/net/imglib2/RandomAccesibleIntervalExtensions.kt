9d6ad8f7c39f257601f59bd639b3e7a2e7e43d17
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-03-02 13:21:10 -0500

    Add basic scalar arithmetic for RAI

--- src/main/kotlin/net/imglib2/RandomAccesibleIntervalExtensions.kt
+++ src/main/kotlin/net/imglib2/RandomAccesibleIntervalExtensions.kt
@@ -8,6 +8,16 @@ import net.imglib2.type.logic.BitType
 import net.imglib2.type.numeric.IntegerType
 import net.imglib2.type.numeric.NumericType
 import net.imglib2.type.numeric.RealType
+import net.imglib2.type.numeric.integer.createVariable
+import net.imglib2.type.numeric.integer.divAssign
+import net.imglib2.type.numeric.integer.minusAssign
+import net.imglib2.type.numeric.integer.plusAssign
+import net.imglib2.type.numeric.integer.timesAssign
+import net.imglib2.type.numeric.real.createVariable
+import net.imglib2.type.numeric.real.divAssign
+import net.imglib2.type.numeric.real.minusAssign
+import net.imglib2.type.numeric.real.plusAssign
+import net.imglib2.type.numeric.real.timesAssign
 import net.imglib2.type.operators.ValueEquals
 import net.imglib2.util.Intervals
 import net.imglib2.util.Util
@@ -15,12 +25,8 @@ import net.imglib2.view.Views
 import java.util.function.BiConsumer
 import java.util.function.Predicate
 
-import net.imglib2.type.numeric.integer.createVariable
-import net.imglib2.type.numeric.integer.plusAssign
-import net.imglib2.type.numeric.real.createVariable
-import net.imglib2.type.numeric.real.plusAssign
 
-
+// plus
 operator fun <T: NumericType<T>> RandomAccessibleInterval<T>.plusAssign(other: RandomAccessibleInterval<T>) {
     require(numDimensions() == other.numDimensions(), {"Num dimensions mismatch: $this $other"})
     (0 until numDimensions()).forEach { require(dimension(it) == other.dimension(it), {"Dimension $it mismatch: $this $other"}) }
@@ -33,6 +39,46 @@ operator fun <T: NumericType<T>> RandomAccessibleInterval<T>.plus(other: RandomA
     return if (isZeroMin()) copy else Views.translate(copy, *this.minAsLongs())
 }
 
+// minus
+operator fun <T: NumericType<T>> RandomAccessibleInterval<T>.minusAssign(other: RandomAccessibleInterval<T>) {
+    require(numDimensions() == other.numDimensions(), {"Num dimensions mismatch: $this $other"})
+    (0 until numDimensions()).forEach { require(dimension(it) == other.dimension(it), {"Dimension $it mismatch: $this $other"}) }
+    LoopBuilder.setImages(this, other).forEachPixel(BiConsumer{ t, o -> t.sub(o)})
+}
+
+operator fun <T: NumericType<T>> RandomAccessibleInterval<T>.minus(other: RandomAccessibleInterval<T>): RandomAccessibleInterval<T> {
+    val copy = copy()
+    copy -= other
+    return if (isZeroMin()) copy else Views.translate(copy, *this.minAsLongs())
+}
+
+// times
+operator fun <T: NumericType<T>> RandomAccessibleInterval<T>.timesAssign(other: RandomAccessibleInterval<T>) {
+    require(numDimensions() == other.numDimensions(), {"Num dimensions mismatch: $this $other"})
+    (0 until numDimensions()).forEach { require(dimension(it) == other.dimension(it), {"Dimension $it mismatch: $this $other"}) }
+    LoopBuilder.setImages(this, other).forEachPixel(BiConsumer{ t, o -> t.mul(o)})
+}
+
+operator fun <T: NumericType<T>> RandomAccessibleInterval<T>.times(other: RandomAccessibleInterval<T>): RandomAccessibleInterval<T> {
+    val copy = copy()
+    copy *= other
+    return if (isZeroMin()) copy else Views.translate(copy, *this.minAsLongs())
+}
+
+// minus
+operator fun <T: NumericType<T>> RandomAccessibleInterval<T>.divAssign(other: RandomAccessibleInterval<T>) {
+    require(numDimensions() == other.numDimensions(), {"Num dimensions mismatch: $this $other"})
+    (0 until numDimensions()).forEach { require(dimension(it) == other.dimension(it), {"Dimension $it mismatch: $this $other"}) }
+    LoopBuilder.setImages(this, other).forEachPixel(BiConsumer{ t, o -> t.div(o)})
+}
+
+operator fun <T: NumericType<T>> RandomAccessibleInterval<T>.div(other: RandomAccessibleInterval<T>): RandomAccessibleInterval<T> {
+    val copy = copy()
+    copy /= other
+    return if (isZeroMin()) copy else Views.translate(copy, *this.minAsLongs())
+}
+
+// plus
 operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.plusAssign(value: T) = iterable().forEach { it += value }
 operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.plusAssign(value: Int) = plusAssign(createType(value))
 operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.plusAssign(value: Long) = plusAssign(createType(value))
@@ -46,6 +92,51 @@ operator fun <T: RealType<T>> RandomAccessibleInterval<T>.plusAssign(value: Doub
 operator fun <T: RealType<T>> RandomAccessibleInterval<T>.plus(value: T) = copy().let{ it.iterable().forEach { it += value }; it }
 operator fun <T: RealType<T>> RandomAccessibleInterval<T>.plus(value: Float) = plus(createType(value))
 operator fun <T: RealType<T>> RandomAccessibleInterval<T>.plus(value: Double) = plus(createType(value))
+
+// minus
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.minusAssign(value: T) = iterable().forEach { it -= value }
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.minusAssign(value: Int) = minusAssign(createType(value))
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.minusAssign(value: Long) = minusAssign(createType(value))
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.minus(value: T) = copy().let {it.iterable().forEach { it -= value }; it}
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.minus(value: Int) = minus(createType(value))
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.minus(value: Long) = minus(createType(value))
+
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.minusAssign(value: T) = iterable().forEach { it -= value }
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.minusAssign(value: Float) = minusAssign(createType(value))
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.minusAssign(value: Double) = minusAssign(createType(value))
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.minus(value: T) = copy().let{ it.iterable().forEach { it -= value }; it }
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.minus(value: Float) = minus(createType(value))
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.minus(value: Double) = minus(createType(value))
+
+// times
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.timesAssign(value: T) = iterable().forEach { it *= value }
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.timesAssign(value: Int) = timesAssign(createType(value))
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.timesAssign(value: Long) = timesAssign(createType(value))
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.times(value: T) = copy().let {it.iterable().forEach { it *= value }; it}
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.times(value: Int) = times(createType(value))
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.times(value: Long) = times(createType(value))
+
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.timesAssign(value: T) = iterable().forEach { it *= value }
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.timesAssign(value: Float) = timesAssign(createType(value))
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.timesAssign(value: Double) = timesAssign(createType(value))
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.times(value: T) = copy().let{ it.iterable().forEach { it *= value }; it }
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.times(value: Float) = times(createType(value))
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.times(value: Double) = times(createType(value))
+
+// div
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.divAssign(value: T) = iterable().forEach { it /= value }
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.divAssign(value: Int) = divAssign(createType(value))
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.divAssign(value: Long) = divAssign(createType(value))
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.div(value: T) = copy().let {it.iterable().forEach { it /= value }; it}
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.div(value: Int) = div(createType(value))
+operator fun <T: IntegerType<T>> RandomAccessibleInterval<T>.div(value: Long) = div(createType(value))
+
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.divAssign(value: T) = iterable().forEach { it /= value }
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.divAssign(value: Float) = divAssign(createType(value))
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.divAssign(value: Double) = divAssign(createType(value))
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.div(value: T) = copy().let{ it.iterable().forEach { it /= value }; it }
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.div(value: Float) = div(createType(value))
+operator fun <T: RealType<T>> RandomAccessibleInterval<T>.div(value: Double) = div(createType(value))
 
 fun <T> RandomAccessibleInterval<T>.minAsLongs() = Intervals.minAsLongArray(this)
 fun <T> RandomAccessibleInterval<T>.maxAsLongs() = Intervals.maxAsLongArray(this)
--- src/test/kotlin/net/imglib2/RandomAccessibleExtensionsKtTest.kt
+++ src/test/kotlin/net/imglib2/RandomAccessibleExtensionsKtTest.kt
@@ -3,9 +3,11 @@ package net.imglib2
 import net.imglib2.converter.Converters
 import net.imglib2.img.array.ArrayImgs
 import net.imglib2.type.numeric.integer.IntType
-import net.imglib2.type.numeric.integer.LongType
+import net.imglib2.type.numeric.integer.minus
 import net.imglib2.type.numeric.real.DoubleType
 import net.imglib2.type.numeric.real.FloatType
+import net.imglib2.type.numeric.real.divAssign
+import net.imglib2.type.numeric.real.minusAssign
 import net.imglib2.util.Intervals
 import net.imglib2.view.Views
 import org.junit.Assert
@@ -99,7 +101,136 @@ class RandomAccessibleExtensionsKtTest {
             zeros += FloatType(3.0F)
             Assert.assertTrue(zeros.contentsEqual(6.0).all())
         }
+    }
 
+    @Test
+    fun minusTest() {
+        val rng = Random(100L)
+        val img1 = ArrayImgs.ints(1, 2, 3) as RandomAccessibleInterval<IntType>
+        val img2 = ArrayImgs.ints(1, 2, 3) as RandomAccessibleInterval<IntType>
+        img1.iterable().forEach { it.set(rng.nextInt(1, 10)) }
+        img2.iterable().forEach { it.set(rng.nextInt(1, 10)) }
+        Assert.assertFalse(img1.contentsEqual(0).all())
+        Assert.assertFalse(img2.contentsEqual(0).all())
+        val diff1 = img1.copy()
+        diff1 -= img2
+        val diff2 = img1 - img2
+        Assert.assertTrue(diff1.contentsEqual(diff2).all())
+        Assert.assertTrue(diff1.contentsEqual(Views.interval(Converters.convert(Views.pair(img1, img2), { s, t -> t.set(s.a - s.b)}, IntType()), img1)).all())
+
+        ArrayImgs.ints(1, 2, 3).let { zeros ->
+            Assert.assertTrue(zeros.contentsEqual(0).all())
+            Assert.assertTrue((zeros - IntType(-1)).contentsEqual(1).all())
+            Assert.assertTrue((zeros - 1).contentsEqual(-1).all())
+            Assert.assertTrue((zeros - 2L).contentsEqual(-2).all())
+
+            zeros -= 1
+            Assert.assertTrue(zeros.contentsEqual(-1).all())
+            zeros -= 2L
+            Assert.assertTrue(zeros.contentsEqual(-3).all())
+            zeros -= IntType(3)
+            Assert.assertTrue(zeros.contentsEqual(-6).all())
+        }
+
+        ArrayImgs.floats(1, 2, 3).let { zeros ->
+            Assert.assertTrue(zeros.contentsEqual(0.0F).all())
+            Assert.assertTrue((zeros - FloatType(-1.0F)).contentsEqual(1.0).all())
+            Assert.assertTrue((zeros - 1.0F).contentsEqual(-1.0).all())
+            Assert.assertTrue((zeros - 2.0).contentsEqual(-2.0).all())
+
+            zeros -= 1.0
+            Assert.assertTrue(zeros.contentsEqual(-1.0).all())
+            zeros -= 2.0F
+            Assert.assertTrue(zeros.contentsEqual(-3.0).all())
+            zeros -= FloatType(3.0F)
+            Assert.assertTrue(zeros.contentsEqual(-6.0).all())
+        }
+    }
+
+    @Test
+    fun timesTest() {
+        val rng = Random(100L)
+        val img = ArrayImgs.ints(1, 2, 3) as RandomAccessibleInterval<IntType>
+        img.iterable().forEach { it.set(rng.nextInt(1, 10)) }
+        Assert.assertFalse(img.contentsEqual(0).all())
+        val sum1 = img.copy()
+        sum1 *= img
+        val sum2 = img * img
+        Assert.assertTrue(sum1.contentsEqual(sum2).all())
+        Assert.assertFalse(sum1.contentsEqual(0).any())
+        Assert.assertTrue(sum1.contentsEqual(Converters.convert(img, {s, t -> t.set(s.get() * s.get())}, IntType())).all())
+
+        ArrayImgs.ints(1, 2, 3).plus(1).let { ones ->
+            Assert.assertTrue(ones.contentsEqual(1).all())
+            Assert.assertTrue((ones * IntType(-1)).contentsEqual(-1).all())
+            Assert.assertTrue((ones * 3).contentsEqual(3).all())
+            Assert.assertTrue((ones * 2L).contentsEqual(2).all())
+
+            ones *= 1
+            Assert.assertTrue(ones.contentsEqual(1).all())
+            ones *= 2L
+            Assert.assertTrue(ones.contentsEqual(2).all())
+            ones *= IntType(3)
+            Assert.assertTrue(ones.contentsEqual(6).all())
+        }
+
+        ArrayImgs.floats(1, 2, 3).plus(1.0F).let { ones ->
+            Assert.assertTrue(ones.contentsEqual(1.0F).all())
+            Assert.assertTrue((ones * FloatType(-1.0F)).contentsEqual(-1.0).all())
+            Assert.assertTrue((ones * 3.0F).contentsEqual(3.0).all())
+            Assert.assertTrue((ones * Math.PI).contentsEqual(Math.PI).all())
+
+            ones *= 1.0
+            Assert.assertTrue(ones.contentsEqual(1.0).all())
+            ones *= 2.0F
+            Assert.assertTrue(ones.contentsEqual(2.0).all())
+            ones *= FloatType(3.0F)
+            Assert.assertTrue(ones.contentsEqual(6.0).all())
+        }
+    }
+
+    @Test
+    fun divTest() {
+        val rng = Random(100L)
+        val img1 = ArrayImgs.ints(1, 2, 3) as RandomAccessibleInterval<IntType>
+        val img2 = ArrayImgs.ints(1, 2, 3) as RandomAccessibleInterval<IntType>
+        img1.iterable().forEach { it.set(rng.nextInt(1, 10)) }
+        img2.iterable().forEach { it.set(rng.nextInt(1, 10)) }
+        Assert.assertFalse(img1.contentsEqual(0).all())
+        Assert.assertFalse(img2.contentsEqual(0).all())
+        val diff1 = img1.copy()
+        diff1 /= img2
+        val diff2 = img1 / img2
+        Assert.assertTrue(diff1.contentsEqual(diff2).all())
+        Assert.assertTrue(diff1.contentsEqual(Views.interval(Converters.convert(Views.pair(img1, img2), { s, t -> t.set(s.a); t.divAssign(s.b)}, IntType()), img1)).all())
+
+        ArrayImgs.ints(1, 2, 3).plus(10).let { tens ->
+            Assert.assertTrue(tens.contentsEqual(10).all())
+            Assert.assertTrue((tens / IntType(-1)).contentsEqual(-10).all())
+            Assert.assertTrue((tens / 2).contentsEqual(5).all())
+            Assert.assertTrue((tens / 3L).contentsEqual(3).all())
+
+            tens /= 2
+            Assert.assertTrue(tens.contentsEqual(5).all())
+            tens /= -3L
+            Assert.assertTrue(tens.contentsEqual(-1).all())
+            tens /= IntType(4)
+            Assert.assertTrue(tens.contentsEqual(0).all())
+        }
+
+        ArrayImgs.floats(1, 2, 3).plus(1.0F).let { ones ->
+            Assert.assertTrue(ones.contentsEqual(1.0F).all())
+            Assert.assertTrue((ones / FloatType(-1.0F)).contentsEqual(-1.0).all())
+            Assert.assertTrue((ones / 3.0F).contentsEqual(1.0 / 3.0).all())
+            Assert.assertTrue((ones / 2.0).contentsEqual(0.5).all())
+
+            ones /= -2.0
+            Assert.assertTrue(ones.contentsEqual(-0.5).all())
+            ones /= 3.0F
+            Assert.assertTrue(ones.contentsEqual(-0.5 / 3.0).all())
+            ones /= FloatType(-4.0F)
+            Assert.assertTrue(ones.contentsEqual(0.5 / 3.0 / 4.0).all())
+        }
     }
 
 }
