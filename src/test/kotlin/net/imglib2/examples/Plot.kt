1bb92a3227dbb320ca3af47a47d3fa222a453f3e
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-03-02 16:09:05 -0500

    Enhance plot example

--- src/test/kotlin/net/imglib2/examples/Plot.kt
+++ src/test/kotlin/net/imglib2/examples/Plot.kt
@@ -8,26 +8,28 @@ import net.imglib2.iterable
 import net.imglib2.minus
 import net.imglib2.times
 import net.imglib2.unaryMinus
-import org.knowm.xchart.QuickChart
 import org.knowm.xchart.SwingWrapper
+import org.knowm.xchart.XYChartBuilder
+import org.knowm.xchart.style.Styler
 import kotlin.math.sqrt
+
 
 fun main() {
     val n = 1000
     val X = ArrayImgs.doubles(n.toLong()).let { it.forEachIndexed { index, t -> t.setReal(index.toDouble()) }; it } / n.toDouble() * 2.0 - 1.0
     val center = 0.3
-    val sigma  = 0.4
-    val sigmaSq = sigma * sigma
-    val diff = center - X
-    val norm = 1.0 /  sqrt(2 * Math.PI * sigmaSq)
-    val y = norm * (-diff * diff / (2 * sigmaSq)).exp()
-    val chart = QuickChart.getChart(
-            "Normal Distribution density function",
-            "X",
-            "p(X)",
-            "p(X)",
-            X.iterable().map { it.realDouble }.toDoubleArray(),
-            y.iterable().map { it.realDouble }.toDoubleArray())
+    val chart = XYChartBuilder()
+            .title("Density of normal distribution in interval [-1, 1] for various sigmas")
+            .width(800)
+            .height(600)
+            .theme(Styler.ChartTheme.Matlab).build()
+    for (sigma in doubleArrayOf(0.1, 0.3, 0.9, 1.5)) {
+        val sigmaSq = sigma * sigma
+        val diff = center - X
+        val norm = 1.0 / sqrt(2 * Math.PI * sigmaSq)
+        val y = norm * (-diff * diff / (2 * sigmaSq)).exp()
+        chart.addSeries("Sigma=$sigma", X.iterable().map { it.realDouble }.toDoubleArray(), y.iterable().map { it.realDouble }.toDoubleArray())
+    }
     SwingWrapper(chart).displayChart()
 
 }
